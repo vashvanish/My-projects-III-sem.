5991cb43e2a4ce91b875eb1a2cf6bcba65cad86a
Ivan Stetsiuk <42550626+vashvanish@users.noreply.github.com>
2019-11-07 00:36:58 +0300

    Update Some_Moving_Balls_Pro.cpp

--- Some_Moving_Balls_Pro.cpp
+++ Some_Moving_Balls_Pro.cpp
@@ -25,7 +25,7 @@ void moveBall(float *x, float *y, float *Vx, float *Vy)
     }
 void ballscollision(float *x_a, float *y_a, float r_a, float *Vx_a, float *Vy_a, float *x_b, float *y_b, float r_b, float *Vx_b, float *Vy_b)
     {
-        if (((*x_b - *x_a) * (*x_b - *x_a) + (*y_b - *y_a) * (*y_b - *y_a)) == (r_b+r_a)*(r_b+r_a))
+        if (((*x_b - *x_a) * (*x_b - *x_a) + (*y_b - *y_a) * (*y_b - *y_a)) <= ((r_b + r_a) * (r_b + r_a)))
             *Vx_a = -*Vx_a;
             *Vy_a = -*Vy_a;
             *Vx_b = -*Vx_b;
@@ -61,22 +61,19 @@ int main()
     for(; ;)
 {
     txClear();
+        
     drawBall(x_1, y_1, r_1, 255, 0, 0);
     drawBall(x_2, y_2, r_2, 255, 0, 100);
     drawBall(x_3, y_3, r_3, 0, 255, 255);
 
     moveBall(&x_1, &y_1, &Vx_1, &Vy_1);
     moveBall(&x_2, &y_2, &Vx_2, &Vy_2);
-
-    ballscollision(&x_1, &y_1, r_1, &Vx_1, &Vy_1, &x_2, &y_2, r_2, &Vx_2, &Vy_2);
-
     moveBall(&x_3, &y_3, &Vx_3, &Vy_3);
 
+    ballscollision(&x_1, &y_1, r_1, &Vx_1, &Vy_1, &x_2, &y_2, r_2, &Vx_2, &Vy_2);
     ballscollision(&x_1, &y_1, r_1, &Vx_1, &Vy_1, &x_3, &y_3, r_3, &Vx_3, &Vy_3);
     ballscollision(&x_3, &y_3, r_3, &Vx_3, &Vy_3, &x_2, &y_2, r_2, &Vx_2, &Vy_2);
-
-
-
+        
     txSleep(dt);
 }
     return 0;
